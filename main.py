939f79662a9b34108e9e2c39f8927dc0e58c557d
Avi0n <14863961+Avi0n@users.noreply.github.com>
2019-05-29 13:14:44 -0700

    Not running as root user anymore

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ load_dotenv()
 
 # Initialize logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
