0f6ed92f2c58d2b129da8ee0a257c88d7378d682
Avi0n <14863961+Avi0n@users.noreply.github.com>
2020-05-09 17:45:29 -0700

    Seem to have skipped 3.2

--- bot/main.py
+++ bot/main.py
@@ -876,7 +876,7 @@ def button(update, context):
 
 
 def main():
-    print("Starting Aqua 3.3 beta 4.2")
+    print("Starting Aqua 3.2 beta 4.2")
     # Check to see if db folder exists
     if Path("db").exists() is True:
         pass
